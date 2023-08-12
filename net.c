35257ebba800b75ed9dfb70508a52f7ac916a226
scrwnl <63852665+scrwnl@users.noreply.github.com>
2023-08-12 23:36:42 +0900

    Update net.c net.h

---
+++ net.c
@@ -0,0 +1,26 @@
+#include "net.h"
+
+
+static struct net_device *devices;
+
+struct net_device *net_device_alloc(void) {
+    struct net_device *dev;
+
+    dev = memory_alloc(sizeof(*dev));
+    if (!dev) {
+        errorf("memory_alloc() failure");
+        return NULL;
+    }
+    return dev;
+}
+
+int net_device_resister(struct net_device *dev) {
+    static unsigned int index = 0;
+
+    dev->index = index++;
+    snprintf(dev->name, sizeof(dev->name), "net%d", dev->index);
+    dev->next = devices;
+    devices = dev;
+    infof("registered, dev=%s, type=0x%04x", dev->name, dev->type);
+    return 0;
+}
